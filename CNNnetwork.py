66ef36bd4111fb5acb0ddd44f78179c4a4914032
hua <hua31415@outlook.com>
2022-11-08 09:38:14 +0800

    加了两个python文件

---
+++ CNNnetwork.py
@@ -0,0 +1,3 @@
+class CNNnetwork:#神经网络算法
+    def CNN(self):
+        pass;
---
+++ Users.py
@@ -0,0 +1,9 @@
+class Users:#定义用户类
+    def __init__(self):
+        Users.sex='LGBT'
+        Users.jacket=[0,0,0,0,0]#用户上衣喜好度列表，依次为T恤。。。。。。
+        Users.pants=[0,0,0,0,0]#用户下衣喜好度列表，依次为牛仔裤。。。。。。
+        Users.outfit=[]#用户穿搭喜好度列表，上衣下衣全排列
+    def recommend(self):#根据属性推荐穿搭的方法
+        pass
+
