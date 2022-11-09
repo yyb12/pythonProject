722944227c6bc4a816f73e2f3967d33e35caa461
hua <hua31415@outlook.com>
2022-11-09 15:09:50 +0800

    实现了用户类

--- CNNnetwork.py
+++ CNNnetwork.py
@@ -1,5 +1,6 @@
 import keras
 import tensorflow as tf
+import numpy as np
 from keras import models,layers
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
@@ -9,4 +10,4 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))#输出层
+model.add(layers.Dense(4))#输出层
--- Users.py
+++ Users.py
@@ -1,9 +1,31 @@
-﻿class Users:#定义用户类
+﻿import os
+def findMax(list):#寻找列表最大值所在的序号
+    max=0
+    index=0
+    for a in range(len(list)):
+        if max<list[a]:
+            max=list[a]
+            index=a
+    return index
+class Users:#定义用户类
     def __init__(self):
         Users.sex='LGBT'
-        Users.jacket=[0,0,0,0,0]#用户上衣喜好度列表，依次为T恤。。。。。。
-        Users.pants=[0,0,0,0,0]#用户下衣喜好度列表，依次为牛仔裤。。。。。。
-        Users.outfit=[]#用户穿搭喜好度列表，上衣下衣全排列
+        Users.jacket=[0,0,0,0]#用户上衣喜好度列表，依次为亮色长袖、暗色长袖、亮色短袖、暗色长袖
+        Users.pants=[0,0,0,0]#用户下衣喜好度列表，依次为亮色长裤、暗色长裤、亮色短裤、暗色短裤
+    def plusjacket(self,index):#上衣喜好度+1
+        Users.jacket[index]=Users.jacket[index]+1
+    def minusjacket(self,index):#上衣喜好度-1
+        Users.jacket[index]=Users.jacket[index]-1
+    def pluspants(self,index):#下衣喜好度+1
+        Users.pants[index]=Users.pants[index]+1
+    def minuspants(self,index):#下衣服喜好度-1
+        Users.pants[index]=Users.pants[index]-1
     def recommend(self):#根据属性推荐穿搭的方法
-        pass
+        favorite_jacket=findMax(Users.jacket)#找到最喜爱的上衣类型
+        favorite_pants=findMax(Users.pants)#找到最喜欢的下衣类型
+        file=open('C:\\Users\\hua31\\Desktop\\softwarepractice\\repository507\\clothes\\example.jpg')
+        return file#返回推荐的图片
+
+
+
 
